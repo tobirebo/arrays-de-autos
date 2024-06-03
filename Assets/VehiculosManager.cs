436c175d4d2024497c5ffb06fd9a3d2b355fa725
tobirebo <48115989@est.ort.edu.ar>
2024-06-03 09:05:43 -0300

    codigo

---
+++ Assets/VehiculosManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehiculosManager : MonoBehaviour
+{
+    public Vehiculo vehiculoScript;
+    public VehiculosManager test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int cantRuedasVehiculo = vehiculoScript.cantRuedas;
+        Debug.Log("Cant ruedas: "+cantRuedasVehiculo);
+        if (cantRuedasVehiculo < 6)
+        {
+            vehiculoScript.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
